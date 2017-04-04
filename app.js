04c73ee67f4dd3838ac3f4835455fbfc761bbbae
xiadd <xiadd0102@gmail.com>
2017-04-05 00:06:43 +0800

    init

---
+++ app.js
@@ -0,0 +1,47 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.checkSession({
+        fail: function () {
+          wx.login({
+            success: function (data) {
+              if (data.code) {
+                wx.request({
+                  url: 'https://dev.xiadd.me/api/authenticate',
+                  data: {
+                    code: data.code
+                  },
+                  success: function(res){
+                    if (res.data.data) {
+                      wx.setStorageSync('token', res.data.data)
+                    }
+                  }
+                })
+              }
+              wx.getUserInfo({
+                success: function (res) {
+                  that.globalData.userInfo = res.userInfo
+                  typeof cb == "function" && cb(that.globalData.userInfo)
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
+  }
+})
